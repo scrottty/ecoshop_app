7b04cc12ac5e5633993fdfb0c13fec0630ae9b0b
scrotty <ollie.steiner@me.com>
2019-10-09 20:36:27 -0700

    Initial Commit

---
+++ lib/customIcons.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons
+{
+  static const IconData menu = Icons.menu;
+  static const IconData option = IconData(0xe902, fontFamily: "CustomIcons");
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'customIcons.dart';
+
+void main() => runApp(MaterialApp
+(
+  home: MyApp(),
+  debugShowCheckedModeBanner: false,
+));
+
+class MyApp extends StatefulWidget
+{
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold
+    (
+      backgroundColor: Color(0xFF2d3447),
+      body: SingleChildScrollView
+      (
+        child: Column
+        (
+          children: <Widget>
+          [
+            Padding
+            (
+              padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
+              child: Row
+              (
+                children: <Widget>
+                [
+                  IconButton
+                  (
+                    icon: Icon
+                    (
+                      CustomIcons.menu,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              )
+            )
+          ]
+        )
+      ),
+    );
+  }
+}
